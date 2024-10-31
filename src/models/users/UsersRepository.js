d4040ccf20c1d5809858810df1378c03aacb73f9
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2024-10-31 09:48:28 -0300

    create add users method

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -1,13 +1,19 @@
 import User from './User.js';
 
-class UsersRepository { 
- 
-        constructor(){
-            this.users = [];
+class UsersRepository {
+
+    constructor() {
+        this.users = [];
     }
 
     getAllUsers() {
         return this.users;
+    }
+    addUser(name, email, password) {
+        const newUser = new User(name, email, password);
+
+        this.users.push(newUser);
+        return newUser;
     }
 }
 

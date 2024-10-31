a59b22c701289fed8d51df2a993501454d65173f
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2024-10-31 08:49:25 -0300

    create user class

---
+++ src/models/users/User.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(name, email, password) {
+        this.id = this.genereateId();
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+    genereateId() {
+       return Math.floor(Math.random() * 999) + 1;
+    }
+}

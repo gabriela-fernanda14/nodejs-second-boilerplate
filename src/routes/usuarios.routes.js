7a03d552b64bcdf819341f638b9a5a9bd3aab98d
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2024-10-31 09:16:23 -0300

    create users repository

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 
+import UsersRepository from "../models/users/UsersRepository.js";
+
 const usuariosRoutes = Router();
+
+const usersRepository = new UsersRepository();
 
 let usuarios = [];
 
 usuariosRoutes.get ("/", (req, res) => {
+    const usarios = usersRepository.getAllUsers();
+
+
     return res.status(200).json({
         message:
         usuarios.length == 0
